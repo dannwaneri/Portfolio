12b1ec0dc7e80bd3f30239264a2247e4148ee74b
Penuelnwaneri <penuelchudi@gmail.com>
2021-07-29 21:45:18 +0100

    updated BlogList

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,7 +6,7 @@ const BlogList = ({ blogs, title,handleDelete}) => {
           <div className="blog-preview" key={blog.id}>
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
-            <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+            <button style={{display:'none'}} onClick={() => handleDelete(blog.id)}>Delete Blog</button>
           </div>
         ))}
       </div>
