3e3a757509495033d1df725d1535961353e5aab2
Penuel Nwaneri <penuelnwaneri@gmail.com>
2021-08-12 10:50:55 +0100

    Update Sun.js

--- src/icons/Sun.js
+++ src/icons/Sun.js
@@ -1,6 +1,6 @@
 const Moonicon = () => {
     return (  
-        <svg style ={{fill:'none',stroke:'currentColor',width:'1.89rem',height:'1.89rem',display:'block'}} viewBox="0 0 24 24">
+        <svg style ={{fill:'none',stroke:'currentColor',width:'1.7rem',height:'1.7rem',display:'block'}} viewBox="0 0 24 24">
                         <path style ={{strokeLinecap:'round',strokeLinejoin:'round',strokeWidth:2,}} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z">
                     </path>
                     </svg>
